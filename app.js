dbbb712f0e5670e07ffd04f62802074d3d16bb87
Ubuntu <ubuntu@ip-172-31-23-178.ec2.internal>
2025-05-11 12:40:57 +0000

    Added Dockerfile, docker-compose.yml, and updated app.js

--- app.js
+++ app.js
@@ -6,93 +6,89 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//Connecting to database
-main().catch(err => console.log(err));
+// Connecting to database
+main().catch(err => console.log("MongoDB connection error:", err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/blogDB', { useNewUrlParser: true });
+  await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/blogDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 }
 
-//let posts = [];
-
-//Creating Schema for the posts
+// Post schema and model
 const postSchema = new mongoose.Schema({
-  title : String,
-  content : String
+  title: String,
+  content: String
 });
 
-//Creating a mongoose model based on this Schema
 const Post = mongoose.model("Post", postSchema);
 
-
-app.get("/", function(req, res){
+// Routes
+app.get("/", function (req, res) {
   Post.find().then(posts => {
     res.render("home", {
-      startingContent: homeStartingContent, 
+      startingContent: homeStartingContent,
       posts: posts
     });
   });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
- //Saved the title and the post into our blogDB database
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
-  //posts.push(post); instead of pushing post to array we are adding it to db.
-  post.save().then(() => {
-    console.log('Post added to DB.');
-    res.redirect('/');
-  })
-  .catch(err => {
-    res.status(400).send("Unable to save post to database.")
-  });
-
+  post.save()
+    .then(() => {
+      console.log('Post added to DB.');
+      res.redirect('/');
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save post to database.");
+    });
 });
 
-app.get("/posts/:postId", function(req, res){
-
-  //We are storing the _id of our created post in a variable named requestedPostId
+app.get("/posts/:postId", function (req, res) {
   const requestedPostId = req.params.postId;
 
-  Post.findOne({_id:requestedPostId})
-  .then(function (post) {
-    res.render("post", {
-            title: post.title,
-            content: post.content
-          });
+  Post.findOne({ _id: requestedPostId })
+    .then(function (post) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
-    })
-
-  });
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    });
 });
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
+});
+
